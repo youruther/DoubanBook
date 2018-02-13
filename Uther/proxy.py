da8d52f856720bb13849202b58bd093eb76c5565
superbaoliang@163.com <superbaoliang@163.com>
2018-02-13 09:59:28 +0800

    add file

--- Uther/proxy.py
+++ Uther/proxy.py
@@ -1,16 +1,30 @@
 import sqlite3
+import os
 
 
 class Proxy(object):
+    cx = None
+    cur = None
 
     def __init__(self):
-        print('start proxy')
+        proxy_file = "..\\proxy.sqlite"
+        if not os.path.isfile(proxy_file):
+            print(proxy_file, '不存在')
+            raise AssertionError
+
+        self.cx = sqlite3.connect(proxy_file)
+        self.cur = self.cx.cursor()
 
     def __del__(self):
-        print('end proxy')
+        self.cx.commit()
+        self.cur.close()
+        self.cx.close()
 
     def get_proxy(self):
-        print('get proxy')
+        url = 'SELECT t.IP FROM T_Proxy t WHERE t.Available = \'Y\' ORDER BY RANDOM() LIMIT 1'
+        self.cur.execute(url)
+        rows = self.cur.fetchall()
+        return rows[0][0]
 
 
 g_proxy = Proxy()
